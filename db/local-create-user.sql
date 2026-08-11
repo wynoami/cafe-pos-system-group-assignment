6eed8b3d4c2ad6904755a03e0f1ac71a6137d254
chepeniv <jose.olmos@atlasschool.com>
2026-08-11 06:12:05 -0500

    +local-access-mysql.sh and updated pwd

--- db/local-create-user.sql
+++ db/local-create-user.sql
@@ -1,4 +1,4 @@
 DROP USER IF EXISTS 'cs_dev'@'localhost';
-CREATE USER 'cs_dev'@'localhost' IDENTIFIED BY 'MY$4L-310W$' ;
+CREATE USER 'cs_dev'@'localhost' IDENTIFIED BY '73$7D41V3' ;
 GRANT ALL PRIVILEGES ON FourthWallCafe.* TO 'cs_dev'@'localhost';
 FLUSH PRIVILEGES;
