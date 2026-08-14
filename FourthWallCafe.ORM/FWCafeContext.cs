7fb96912c01718d2760522570664fc59d7e904e2
chepeniv <jose.olmos@atlasschool.com>
2026-08-13 21:11:24 -0500

    +class FWCafeContext from DbContext

---
+++ FourthWallCafe.ORM/FWCafeContext.cs
@@ -0,0 +1,31 @@
+namespace FourthWallCafe.ORM;
+
+using Microsoft.EntityFrameworkCore;
+using FourthWallCafe.LIB;
+using FourthWallCafe.LIB.Entities;
+
+public class FWCafeContext : DbContext
+{
+    private readonly string AccessInfo;
+
+    public FWCafeContext() =>
+        AccessInfo = DbAccess.Credentials;
+
+    public FWCafeContext(string Credentials) =>
+        AccessInfo = Credentials;
+
+    public DbSet<CafeOrder>   CafeOrder   { get; set; }
+    public DbSet<Category>    Category    { get; set; }
+    public DbSet<Item>        Item        { get; set; }
+    public DbSet<ItemPrice>   ItemPrice   { get; set; }
+    public DbSet<OrderItem>   OrderItem   { get; set; }
+    public DbSet<PaymentType> PaymentType { get; set; }
+    public DbSet<Server>      Server      { get; set; }
+    public DbSet<TimeOfDay>   TimeOfDay   { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder Builder)
+    {
+        Builder.UseMySql(AccessInfo, ServerVersion.AutoDetect(AccessInfo));
+    }
+
+}
